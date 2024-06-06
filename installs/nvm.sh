d9c74b7c63f46b5113f8c35add0563d67ee2d680
NVolcz <niklas.volcz@gmail.com>
2024-06-06 19:54:27 +0200

    Small refactor

--- installs/nvm.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-set -euo pipefail
-
-# From: https://github.com/nvm-sh/nvm#install--update-script
-
-curl -fsSL https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.2/install.sh | bash
